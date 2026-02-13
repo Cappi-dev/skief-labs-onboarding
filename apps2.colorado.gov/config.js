7bfd6d69bfdf91a9f6d002c22b8592501b444345
Jes Emanuel Chavez <chavezjes71@gmail.com>
2026-02-13 17:24:55 +0800

    challenge 1.0

--- apps2.colorado.gov/config.js
+++ apps2.colorado.gov/config.js
@@ -1,7 +1,7 @@
-// config.js
+
 module.exports = {
-    // PASTE YOUR API KEY HERE
-    TWO_CAPTCHA_API_KEY: '6d881536e9bf2da434a9d63e18c8ba00', 
+    
+    TWO_CAPTCHA_API_KEY: 'API_KEY_HERE', 
     
     BASE_URL: 'https://apps2.colorado.gov/dora/licensing/lookup/licenselookup.aspx',
     DETAIL_URL_PREFIX: 'https://apps2.colorado.gov/dora/licensing/Lookup/licensedetail.aspx?id=',
